8a6073f6e6b53b0b05d03585fd824ddac3dc5e66
Michal Szorád <miso.szorad@gmail.com>
2017-10-11 20:50:26 +0200

    Simple image lazy load

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// simple image lazyload
+[].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function () {
+        img.removeAttribute('data-src');
+    };
+});
