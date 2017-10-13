2c1491ca15f92850dee34df1f28887c0a395678d
Michal Szorád <miso.szorad@gmail.com>
2017-10-13 14:12:00 +0200

    Post build process logs updated

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -16,11 +16,12 @@ const HTML_FILE_SOURCE = path.join(BUILD_DIR, 'index.html');
 const CORE_CSS_SOURCE = path.join(BUILD_DIR, 'css', 'core.min.css');
 
 
-// inline core css
+// inline core css to speed up the paint time
 const options = {
     '<core-styles/>': `<style>${fs.readFileSync(CORE_CSS_SOURCE).toString()}</style>`
 };
 
+console.log('post build process');
 inlineSources(HTML_FILE_SOURCE, options);
 
 // move build files from build folder to public polder
@@ -30,5 +31,5 @@ copySubdirectories(BUILD_DIR, PUBLIC_DIR)
         console.log('Removing build folder');
         return removeDir(BUILD_DIR);
     })
-    .then(() => console.log('DONE'))
+    .then(() => console.log('postbuild DONE'))
     .catch(error => console.log('Error while moving build files', error));
